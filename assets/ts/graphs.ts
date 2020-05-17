148b49057acd6a1da362516c9da9fcbf56f2fc1d
Chris Chester <chrisjchester@gmail.com>
2020-05-17 20:50:32 +0000

    Graph close to ddone

--- assets/ts/graphs.ts
+++ assets/ts/graphs.ts
@@ -1,16 +1,18 @@
-import * as d3 from 'd3';
+import { Chart, ChartPoint } from 'chart.js';
+import 'chartjs-plugin-colorschemes';
 import { getGoalsData, gameData, scorerData } from "./apiQueries";
 import * as _ from 'lodash';
 
-export type playerScoringRecord = {
-  name: string,
-  matches: matchGoals[]
+export type chartData = {
+  label: string,
+  fill: boolean,
+  data: matchGoals[]
 };
 
 export type matchGoals = {
-  date: Date,
-  matchGoals: number,
-  totalGoals: number
+  t: Date,
+  goals: number,
+  y: number
 };
 
 let parsePlayerData = function (data: gameData[]) {
@@ -19,33 +21,40 @@ let parsePlayerData = function (data: gameData[]) {
   let scorerNames = _.sortedUniq(_.map(temp2, "scorer"));
 
   let playerData = scorerNames.map(name => {
-    let record: playerScoringRecord = {
-      "name": name,
-      "matches": []
+    let record: chartData = {
+      "label": name,
+      "fill": false,
+      "data": []
     }
     return record;
   });
 
-  data = data.sort((a,b) => {
+  data = data.sort((a, b) => {
     return a.date.getTime() - b.date.getTime()
   })
 
   data.forEach(game => {
-    game.scorers.forEach((scorer) => {
-      let index = playerData.findIndex(e => e.name == scorer.scorer)
+    scorerNames.forEach((scorer) => {
+      let playerIndex = playerData.findIndex(e => e.label == scorer)
+      let scorerIndex = game.scorers.findIndex(e => e.scorer == scorer)
 
-      let lastMatch = playerData[index].matches[playerData[index].matches.length-1] || {totalGoals : 0};
-      let prevTotal = lastMatch.totalGoals;
+      let lastMatch = playerData[playerIndex].data[playerData[playerIndex].data.length - 1] || { y: 0 };
+      let prevTotal = lastMatch.y;
 
-      playerData[index].matches.push({
-        "date": game.date,
-        "matchGoals": scorer.goals,
-        "totalGoals": prevTotal + scorer.goals
+      let goals = 0;
+
+      if (scorerIndex > -1) {
+        goals = game.scorers[scorerIndex].goals
+      }
+
+      playerData[playerIndex].data.push({
+        "t": game.date,
+        "goals": goals,
+        "y": prevTotal + goals
       })
     })
   })
 
-  console.log("playerData",playerData)
   return playerData;
 }
 
@@ -55,79 +64,54 @@ let parsePlayerData = function (data: gameData[]) {
 export let populateGsGraph = async function () {
   let data = await getGoalsData();
   let playerData = parsePlayerData(data);
+  console.log("playerData", playerData);
 
-  var lineData = playerData[1].matches;
+  let temp = <HTMLCanvasElement>document.getElementById("stats-panel");
+  let ctx = temp.getContext("2d");
 
-  lineData.sort(function (a, b) {
-    return new Date(b.date).getTime() - new Date(a.date).getTime();
-  });
-
-  var height = 200;
-  var width = 700;
-
-  var margin = { top: 20, right: 15, bottom: 25, left: 25 };
-
-  width = width - margin.left - margin.right;
-  height = height - margin.top - margin.bottom;
-
-  var svg = d3.select('#stats-panel')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // set the ranges
-  var x = d3.scaleTime().range([0, width]);
-
-  x.domain(d3.extent(lineData, function (d) { return d.date; }));
-
-  var y = d3.scaleLinear().range([height, 0]);
-
-  y.domain(d3.extent(lineData, function (d) { return d.totalGoals; }));
-
-  var valueline = d3.line()
-    .x(function (d: any) { return x(d.date); })
-    .y(function (d: any) { return y(d.totalGoals); })
-    .curve(d3.curveMonotoneX);
-
-  svg.append("path")
-    .data([lineData])
-    .attr("class", "line")
-    .attr("d", <any>valueline);
-
-  //  var xAxis_woy = d3.axisBottom(x).tickFormat(d3.timeFormat("Week %V"));
-  var xAxis_woy = d3.axisBottom(x).ticks(11).tickFormat(d3.timeFormat("%y-%b-%d")).tickValues(lineData.map(d => d.date));
-
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis_woy);
-
-  //  Add the Y Axis
-  //  svg.append("g").call(d3.axisLeft(y));
-
-  svg.selectAll(".dot")
-    .data(lineData)
-    .enter()
-    .append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function (d) { return x(d.date) })
-    .attr("cy", function (d) { return y(d.totalGoals) })
-    .attr("r", 5);
-
-  svg.selectAll(".text")
-    .data(lineData)
-    .enter()
-    .append("text") // Uses the enter().append() method
-    .attr("class", "label") // Assign a class for styling
-    .attr("x", function (d, i) { return x(d.date) })
-    .attr("y", function (d) { return y(d.totalGoals) })
-    .attr("dy", "-5")
-    .text(function (d) { return d.totalGoals; });
-
-  // svg.append('text')
-  //   .attr('x', 10)
-  //   .attr('y', -5)
-  //   .text('Almaty');
-
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: playerData
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: 'time'
+        }]
+      },
+      plugins: {
+        colorschemes: {
+          scheme: 'brewer.Paired12'
+        }
+      },
+      tooltips: {
+        callbacks: {
+          title: function(item, data) {
+            console.log("item", item)
+            console.log("data", data)
+            let title = <string> data.datasets[item[0].datasetIndex].label;
+            return title
+          },
+          label: function(item, data) {
+            let dataItem = <matchGoals> data.datasets[item.datasetIndex].data[item.index];
+            let yourDate = dataItem.t;
+            let offset = yourDate.getTimezoneOffset()
+            yourDate = new Date(yourDate.getTime() + (offset * 60 * 1000))
+            return yourDate.toISOString().split('T')[0]
+          },
+          afterLabel: function(item, data) {
+            let dataItem = <matchGoals> data.datasets[item.datasetIndex].data[item.index];
+            return"Total Goals: " + dataItem.y + "\nGameday goals:" + dataItem.goals;
+          }
+        },
+        backgroundColor: '#FFF',
+        titleFontSize: 16,
+        titleFontColor: '#0066ff',
+        bodyFontColor: '#000',
+        bodyFontSize: 14,
+        displayColors: false
+      }
+      }
+    })
 }
